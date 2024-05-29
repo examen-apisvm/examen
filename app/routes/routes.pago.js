e56fcdba5baa30a6f6c992e2698753da8a9cefe2
SamuelVelez17 <juansecuadros344@gmail.com>
2024-05-29 08:00:32 -0500

    Probamos metodos con thunderclient

--- app/routes/routes.pago.js
+++ app/routes/routes.pago.js
@@ -2,9 +2,9 @@ import { Router } from "express";
 
 const rutaPago = Router();
 
-rutaPago.get("/pago", () => {console.log("Probando");});
-rutaPago.post("/pago", () => {console.log("Probando");});
-rutaPago.put("/pago", () => {console.log("Probando");});
-rutaPago.delete("/pago:id", () => {console.log("Probando");});
+rutaPago.get("/pago", () => {console.log("Probando get");});
+rutaPago.post("/pago", () => {console.log("Probando post");});
+rutaPago.put("/pago", () => {console.log("Probando put");});
+rutaPago.delete("/pago:id", () => {console.log("Probando delete");});
 
 export default rutaPago;

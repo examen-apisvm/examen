3d7dbf1a261ba264489b1a93280a8620a6d2bd12
SamuelVelez17 <juansecuadros344@gmail.com>
2024-05-29 08:16:27 -0500

    Se prueban metodos con thunderclient y controllers

---
+++ app/controllers/controllers.pago.js
@@ -0,0 +1,16 @@
+export const mostrarPago = (req, res) => {
+    res.json({"Respuesta": "mostrarPago" })
+};
+
+export const crearPago = (req, res) => {
+    res.json({"Respuesta": "crearPago" })
+};
+
+export const modificarPago = (req, res) => {
+    res.json({"Respuesta": "modificarPago" })
+};
+
+export const eliminarPago = (req, res) => {
+    res.json({"Respuesta": "eliminarPago" })
+};
+
--- app/routes/routes.pago.js
+++ app/routes/routes.pago.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { crearPago, eliminarPago, modificarPago, mostrarPago } from "../controllers/controllers.pago";
 
 const rutaPago = Router();
 
-rutaPago.get("/pago", () => {console.log("Probando get");});
-rutaPago.post("/pago", () => {console.log("Probando post");});
-rutaPago.put("/pago", () => {console.log("Probando put");});
-rutaPago.delete("/pago:id", () => {console.log("Probando delete");});
+rutaPago.get("/pago", mostrarPago);
+rutaPago.post("/pago", crearPago);
+rutaPago.put("/pago", modificarPago);
+rutaPago.delete("/pago:id", eliminarPago);
 
 export default rutaPago;
